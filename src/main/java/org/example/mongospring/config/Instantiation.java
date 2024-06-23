ff7c13cae2ab6b18210678dd820b729098de7826
Felipe <fhora93@gmail.com>
2024-06-23 17:26:54 -0300

    Entity post e referenciando posts do usuario

--- src/main/java/org/example/mongospring/config/Instantiation.java
+++ src/main/java/org/example/mongospring/config/Instantiation.java
@@ -1,12 +1,17 @@
 package org.example.mongospring.config;
 
+import org.example.mongospring.domain.Post;
 import org.example.mongospring.domain.User;
+import org.example.mongospring.dto.AuthorDTO;
 import org.example.mongospring.repository.UserRepository;
+import org.example.mongospring.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 
 @Configuration
@@ -15,8 +20,13 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         userRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -24,5 +34,13 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, sdf.parse("17/12/2004"), "Partiu Viagem", "VOU PARA BENTO. TCHAU", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("29/04/2004"), "Partiu TRIP", "VOU PARA SALVADOR. TCHAU", new AuthorDTO(maria));
+
+        postRepository.saveAll(Arrays.asList(post1,post2));
+        maria.getPosts().addAll(Arrays.asList(post1,post2));
+        userRepository.save(maria);
+
     }
 }
---
+++ src/main/java/org/example/mongospring/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package org.example.mongospring.repository;
+
+import org.example.mongospring.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
